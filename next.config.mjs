9024c19015198b76a4a9c96759c1d3f4b2c5182c
valesit <valentinesithole@yahoo.com>
2026-08-20 13:33:59 -0400

    Allow Wikimedia RAV4 hero image

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
       { protocol: 'https', hostname: '*.supabase.co', pathname: '/storage/**' },
       { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
       { protocol: 'https', hostname: 'images.pexels.com', pathname: '/photos/**' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org', pathname: '/wikipedia/commons/**' },
     ],
   },
 };
