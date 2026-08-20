3ff82776863999507abbc0b15f0e6ca0596c1d1c
valesit <valentinesithole@yahoo.com>
2026-08-20 13:05:25 -0400

    Allow Pexels hero imagery

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '*.supabase.co', pathname: '/storage/**' },
       { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.pexels.com', pathname: '/photos/**' },
     ],
   },
 };
